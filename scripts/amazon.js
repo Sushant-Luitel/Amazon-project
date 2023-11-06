5011991c8a43b2c01ea940b0a264edcc57f835c1
Sushant-Luitel <sushantluitel39@gmail.com>
2023-11-06 12:58:23 +0545

    /images to images

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -14,7 +14,7 @@ products.forEach((product) => {
 <div class="product-rating-container">
   <img
     class="product-rating-stars"
-    src="/images/ratings/rating-${product.rating.stars}.png"
+    src="images/ratings/rating-${product.rating.stars}.png"
   />
   <div class="product-rating-count link-primary">${product.rating.count}</div>
 </div>
