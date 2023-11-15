8d6873399bc0712951ff62eddf10a8d5d8315cc1
Sushant-Luitel <sushantluitel39@gmail.com>
2023-11-15 20:42:06 +0545

    updated select quantity button feature

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,19 @@
 export const cart = [];
 export function addToCart(productId) {
+  const quantity = Number(
+    document.querySelector(`.js-quantity-selector-${productId}`).value
+  );
+
   let matchingItem;
   cart.forEach((cartItem) => {
     if (productId === cartItem.productId) matchingItem = cartItem;
   });
   if (matchingItem) {
-    matchingItem.quantity += 1;
+    matchingItem.quantity += quantity;
   } else {
     cart.push({
       productId: productId,
-      quantity: 1,
+      quantity: quantity,
     });
   }
 }
